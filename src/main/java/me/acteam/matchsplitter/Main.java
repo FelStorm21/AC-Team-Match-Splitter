7f72a0ae5bc2b12c033f964670f005ca1d5f2ac5
Benjamin Zalla <bzalla10@gmail.com>
2018-11-20 21:38:00 -0500

    Command line control flow

--- src/main/java/me/acteam/matchsplitter/Main.java
+++ src/main/java/me/acteam/matchsplitter/Main.java
@@ -33,6 +33,9 @@ public class Main {
 				}
 			} else if (args[0].equals("-path")){
 				dir.setRootPath(Paths.get(args[1]));
+			} else {
+				System.out.println("Invalid input.");
+				isRunning = false;
 			}
 			
 		}
